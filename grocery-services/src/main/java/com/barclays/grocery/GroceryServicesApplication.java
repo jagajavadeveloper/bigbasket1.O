ce0d5e582505a618f66aa5c5de4d8fe4e44c3ce0
Jagadeeswara Patnam <jagadeeswarapatnam99@gmail.com>
2023-06-28 10:08:22 +0530

    project initialization

---
+++ grocery-services/src/main/java/com/barclays/grocery/GroceryServicesApplication.java
@@ -0,0 +1,13 @@
+package com.barclays.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryServicesApplication.class, args);
+	}
+
+}
---
+++ grocery-services/src/test/java/com/barclays/grocery/GroceryServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
